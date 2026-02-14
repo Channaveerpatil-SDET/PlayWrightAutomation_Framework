d71f63753c49b8110f6620974d48f145836594af
Chanaveer Patil <chanaveerpatil@gmail.com>
2026-02-14 14:00:11 +0530

    Loginpages changes

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   /* Run tests in files in parallel */
  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,12 +29,15 @@ export default defineConfig({
   use: {
     browserName: 'chromium',
     headless: false,
+    launchOptions: {
+      args: ['--start-maximized'] // opens browser maximized
+    }
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on-first-retry',
-  },
+  }
 
   /* Configure projects for major browsers */
   // projects: [
--- tests/Uibasics.spec.js
+++ tests/Uibasics.spec.js
@@ -1,31 +1,43 @@
-const {test,expect}= require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 
 
-test('First playwright test', async ({browser}) => 
-
-    {
-     // chrome -Plugins/ cookies
+test.only('First playwright test', async ({ browser }) => {
+      // chrome -Plugins/ cookies
       //edge -Plugins/ cookies
-      const context= await browser.newContext();
+      const context = await browser.newContext();
       const page = await context.newPage();
-      await page.goto("https://rahulshettyacademy.com/learning-paths/");
-            console.log(await page.title());
-         await expect(page).toHaveTitle("QA Learning Paths | QA Automation, AI Testing & SDET Roadmaps");
-      
+      const username = page.locator('input#username');
+      const password = page.locator('input#password');
+      const checkbox = page.locator('input[type="checkbox"]');
+      const signInBtn = page.locator('input#signInBtn');
+      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
-    });
+      console.log(await page.title());
+      await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+      await username.fill("rahulshettyacademy");
+      await password.fill("@830$3mK2");
+      await checkbox.click();
+      await signInBtn.click();
+      //console.log(await page.locator("[style*='block']").textContent());
+      await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+      await page.screenshot({ path: 'screenshot.png', fullPage: true });
+      await password.fill("");
+      await password.fill("Learning@830$3mK2");
+      await signInBtn.click();
+      console.log(await page.locator(".card-body a").first().textContent());
+      await page.screenshot({ path: 'HomeScreenpage.png', fullPage: true });
 
-    test('Second playwright test', async ({page}) => 
+});
 
-    {
-      
+test('Second playwright test', async ({ page }) => {
+
       await page.goto("https://www.irctc.co.in/nget/train-search/");
-      
-       console.log(await page.title());
-       await expect(page).toHaveTitle("IRCTC – Official Indian Railway Catering & Tourism Corporation Portal");
+
+      console.log(await page.title());
+      await expect(page).toHaveTitle("IRCTC – Official Indian Railway Catering & Tourism Corporation Portal");
 
 
-      
 
-    });
+
+});
